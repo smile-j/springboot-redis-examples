be14199ab10698547b0f4c001f866d8819e93297
dongjunpeng <junpeng.dong@causacloud.com>
2019-06-10 18:35:45 +0800

    WebMvcConfigurerAdapter详解

--- springboot-redis-friends/src/main/java/com/haoxy/friends/config/Config.java
+++ springboot-redis-friends/src/main/java/com/haoxy/friends/config/Config.java
@@ -20,4 +20,26 @@ public class Config extends WebMvcConfigurerAdapter {
                 .allowedOrigins("*")
                 .allowCredentials(true);
     }
+    /** 解决跨域问题 **/
+//    public void addCorsMappings(CorsRegistry registry) {}
+
+    /** 添加拦截器 **/
+//    void addInterceptors(InterceptorRegistry registry);
+
+    /** 这里配置视图解析器 **/
+//    void configureViewResolvers(ViewResolverRegistry registry);
+
+    /** 配置内容裁决的一些选项 **/
+//    void configureContentNegotiation(ContentNegotiationConfigurer configurer);
+
+    /** 视图跳转控制器 **/
+//    void addViewControllers(ViewControllerRegistry registry);
+
+    /** 静态资源处理 **/
+//    void addResourceHandlers(ResourceHandlerRegistry registry);
+
+    /** 默认静态资源处理器 **/
+//    void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer);
+
+
 }
